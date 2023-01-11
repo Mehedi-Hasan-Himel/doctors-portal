ed6136eadef07694b3f0c42704c21d532b79eb84
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-11 22:03:26 +0600

    service section responsive

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -34,7 +34,7 @@ const Services = () => {
         </h3>
         <h2 className="text-3xl">Services We Provide</h2>
       </div>
-      <div className="gird lg:grid-cols-3">
+      <div className="mt-5 grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {servicesData.map((service) => (
           <Service key={service.id} service={service}></Service>
         ))}
