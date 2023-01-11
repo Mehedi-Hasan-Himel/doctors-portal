f5f9fee5939675891020116814ef50a5b36ff19f
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-11 21:56:22 +0600

    data added of services section

---
+++ src/Pages/Home/Services/Services.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import fluoride from "../../../assets/images/fluoride.png";
+import whitening from "../../../assets/images/whitening.png";
+import cavity from "../../../assets/images/cavity.png";
+import Service from "./Service";
+
+const Services = () => {
+  const servicesData = [
+    {
+      id: 1,
+      name: `Fluoride Treatment`,
+      description: `Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the`,
+      Img: fluoride,
+    },
+    {
+      id: 2,
+      name: `Teeth Whitening`,
+      description: `Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the`,
+      Img: whitening,
+    },
+    {
+      id: 3,
+      name: `Cavity Filling`,
+      description: `Lorem Ipsum is simply dummy printing and typesetting indust Ipsum has been the`,
+      Img: cavity,
+    },
+  ];
+
+  return (
+    <div className="mt-16">
+      <div className="text-center">
+        <h3 className="text-primary uppercase text-xl font-bold">
+          Our Services
+        </h3>
+        <h2 className="text-3xl">Services We Provide</h2>
+      </div>
+      <div className="gird lg:grid-cols-3">
+        {servicesData.map((service) => (
+          <Service key={service.id} service={service}></Service>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
