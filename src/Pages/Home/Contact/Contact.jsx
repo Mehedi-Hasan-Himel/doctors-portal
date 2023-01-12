86d1504d3ba3464a5f5be4980b963092d7b2747b
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-12 15:03:07 +0600

    contact section

--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import Appointment from "../../../assets/images/appointment.png";
+import PrimaryButton from "../../../Components/PrimaryButton/PrimaryButton";
 
 const Contact = () => {
-   return (
-      <div>
-         <h1>Contact</h1>
+  return (
+    <section
+      className=" py-16"
+      style={{
+        background: `url(${Appointment})`,
+      }}
+    >
+      <div className="text-center w-[450px] mx-auto">
+        <h3 className="text-primary font-bold text-xl">Contact Us</h3>
+        <h2 className="text-4xl	text-white">Stay connected with us</h2>
+        <form className="mt-6 flex flex-col items-center gap-3 ">
+          <input
+            type="text"
+            placeholder="Email Address"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <input
+            type="text"
+            placeholder="Subject"
+            className="input input-bordered w-full	 max-w-xs"
+          />
+          <textarea
+            className="textarea w-9/12	h-32"
+            placeholder="Your Message"
+              ></textarea>
+              <PrimaryButton>Submit</PrimaryButton>
+        </form>
       </div>
-   );
+    </section>
+  );
 };
 
 export default Contact;
