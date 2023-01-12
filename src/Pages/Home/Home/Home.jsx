730bc97f7b8e3dc1e0e0adae95164ab1fc212cad
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-12 13:47:36 +0600

    footer section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../Banner/Banner";
+import Contact from "../Contact/Contact";
 import InfoCards from "../InfoCards/InfoCards";
 import MakeAppooinment from "../MakeAppooinment/MakeAppooinment";
 import Services from "../Services/Services";
@@ -13,6 +14,7 @@ const Home = () => {
       <Services></Services>
       <MakeAppooinment></MakeAppooinment>
       <Testimonial></Testimonial>
+      <Contact></Contact>
     </div>
   );
 };
