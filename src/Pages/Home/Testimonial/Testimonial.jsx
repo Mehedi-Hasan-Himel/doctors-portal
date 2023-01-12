2e5ea421580a8a18d4d245c3554da0c61de4607e
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-12 12:48:19 +0600

    Testimonial

--- src/Pages/Home/Testimonial/Testimonial.jsx
+++ src/Pages/Home/Testimonial/Testimonial.jsx
@@ -10,21 +10,21 @@ const Testimonial = () => {
     {
       _id: 1,
       name: `Winson Herry`,
-      review: ``,
+      review: `It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content`,
       location: `California `,
       Img: people1,
     },
     {
       _id: 2,
       name: `Winson Herry`,
-      review: ``,
+      review: `It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content`,
       location: `California `,
       Img: people2,
     },
     {
       _id: 3,
       name: `Winson Herry`,
-      review: ``,
+      review: `It is a long established fact that by the readable content of a lot layout. The point of using Lorem a more-or-less normal distribu to using Content here, content`,
       location: `California `,
       Img: people3,
     },
@@ -41,10 +41,10 @@ const Testimonial = () => {
           <img className=" w-24 lg:w-48" src={quote} alt="" />
         </figure>
       </div>
-        <div>
-           {
-              reviews.map(review => <Review></Review>)
-           }
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {reviews.map((review) => (
+          <Review key={review._id} review={review}></Review>
+        ))}
       </div>
     </section>
   );
