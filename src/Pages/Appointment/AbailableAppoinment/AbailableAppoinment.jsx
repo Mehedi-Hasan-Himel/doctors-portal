c5d03c71e6121cf32927a2d6d24f48ad115ce573
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-16 22:42:15 +0600

    modal

--- src/Pages/Appointment/AbailableAppoinment/AbailableAppoinment.jsx
+++ src/Pages/Appointment/AbailableAppoinment/AbailableAppoinment.jsx
@@ -1,9 +1,11 @@
 import { format } from "date-fns";
 import React, { useEffect, useState } from "react";
 import AppoinmentOption from "./AppoinmentOption";
+import BookingModal from "../BookingModal/BookingModal";
 
 const AbailableAppoinment = ({ selectedDate, setSelectedDate }) => {
   const [appoinmentOptions, setAppoinmentOptions] = useState([]);
+  const [ treatment, setTreatment] = useState(null)
   useEffect(() => {
     fetch(`appoinmentOptions.json`)
       .then((res) => res.json())
@@ -14,14 +16,16 @@ const AbailableAppoinment = ({ selectedDate, setSelectedDate }) => {
       <p className="text-center text-secondary font-bold">
         Abailable Appoinment on: {format(selectedDate, "PP")}
       </p>
-      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:mx-48	mt-6">
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:mx-48	mt-6 ">
         {appoinmentOptions.map((option) => (
           <AppoinmentOption
             key={option._id}
             appoinmentOption={option}
+            setTreatment={setTreatment}
           ></AppoinmentOption>
         ))}
       </div>
+      <BookingModal></BookingModal>
     </section>
   );
 };
--- src/Pages/Appointment/AbailableAppoinment/AppoinmentOption.jsx
+++ src/Pages/Appointment/AbailableAppoinment/AppoinmentOption.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 
-const AppoinmentOption = ({ appoinmentOption }) => {
+const AppoinmentOption = ({ appoinmentOption, setTreatment }) => {
   const { name, slots } = appoinmentOption;
   return (
     <div className="card shadow-xl">
-      <div className="card-body text-center ">
-        <h2 className="card-title text-secondary font-bold justify-center">{name}</h2>
+      <div className="card-body text-center">
+        <h2 className="card-title text-secondary font-bold justify-center">
+          {name}
+        </h2>
         <p>{slots.length > 0 ? slots[0] : `Try another day.`}</p>
         <p>
           {slots.length} {slots.length <= 1 ? `Space` : `Spaces`} Abailabe
         </p>
         <div className="card-actions justify-center">
-          <button className="btn btn-primary text-white">Book Appoinment</button>
+          <label
+            htmlFor="booking-modal"
+            className="btn btn-primary text-white"
+            onClick={() => setTreatment(appoinmentOption)}
+          >
+            Book Appoinment
+          </label>
         </div>
       </div>
     </div>
