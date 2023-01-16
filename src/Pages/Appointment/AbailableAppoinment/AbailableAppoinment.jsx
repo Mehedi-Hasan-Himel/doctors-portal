fafea073383af58559fa2afcd18a79e7531dfcee
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-16 23:28:29 +0600

    modals dynamic data

--- src/Pages/Appointment/AbailableAppoinment/AbailableAppoinment.jsx
+++ src/Pages/Appointment/AbailableAppoinment/AbailableAppoinment.jsx
@@ -25,7 +25,12 @@ const AbailableAppoinment = ({ selectedDate, setSelectedDate }) => {
           ></AppoinmentOption>
         ))}
       </div>
-      {treatment && <BookingModal treatment={treatment}></BookingModal>}
+      {treatment && (
+        <BookingModal
+          treatment={treatment}
+          selectedDate={selectedDate}
+        ></BookingModal>
+      )}
     </section>
   );
 };
--- src/Pages/Appointment/BookingModal/BookingModal.jsx
+++ src/Pages/Appointment/BookingModal/BookingModal.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import { format } from "date-fns";
+import React from "react";
 
-const BookingModal = ({ treatment }) => {
-  const {name} = treatment
+const BookingModal = ({ treatment, selectedDate }) => {
+  const { name, slots } = treatment; // treatment is appoinment options just different name.
+  const date = format(selectedDate, "PP");
   return (
     <>
       <input type="checkbox" id="booking-modal" className="modal-toggle" />
@@ -13,13 +15,35 @@ const BookingModal = ({ treatment }) => {
           >
             ✕
           </label>
-          <h3 className="text-lg font-bold">
-            {name}
-          </h3>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
+          <h3 className="text-lg font-bold">{name}</h3>
+          <form className="grid grid-cols-1 gap-3 mt-10">
+            <input
+              type="text"
+              value={date}
+              className="input input-bordered w-full"
+            />
+            <select className="select select-bordered ">
+              {slots.map((slot) => (
+                <option value={slot}>{slot}</option>
+              ))}
+            </select>
+            <input
+              type="text"
+              value={date}
+              className="input input-bordered w-full"
+            />
+            <input
+              type="text"
+              value={date}
+              className="input input-bordered w-full"
+            />
+            <br />
+            <input
+              className="w-full btn btn-accent"
+              type="submit"
+              value="Submit"
+            />
+          </form>
         </div>
       </div>
     </>
