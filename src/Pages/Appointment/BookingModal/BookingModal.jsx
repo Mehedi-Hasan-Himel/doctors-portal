b83df560a8e5a88cef1289afc331c3b6ff8bd7d3
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-01-17 00:16:53 +0600

    code

--- src/Pages/Appointment/BookingModal/BookingModal.jsx
+++ src/Pages/Appointment/BookingModal/BookingModal.jsx
@@ -26,15 +26,20 @@ const BookingModal = ({ treatment, selectedDate }) => {
             ✕
           </label>
           <h3 className="text-lg font-bold">{name}</h3>
-          <form className="grid grid-cols-1 gap-3 mt-10">
+          <form
+            onSubmit={handleBooking}
+            className="grid grid-cols-1 gap-3 mt-10"
+          >
             <input
               type="text"
               value={date}
               className="input input-bordered w-full"
             />
             <select className="select select-bordered " name="slot">
-              {slots.map((slot) => (
-                <option value={slot}>{slot}</option>
+              {slots.map((slot, i) => (
+                <option value={slot} key={i}>
+                  {slot}
+                </option>
               ))}
             </select>
             <input
